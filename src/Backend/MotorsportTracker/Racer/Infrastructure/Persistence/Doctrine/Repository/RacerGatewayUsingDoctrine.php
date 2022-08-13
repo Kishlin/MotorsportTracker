232b0b3a41462cd1e87d6e4a13b1ee2b05efd1bd
Kishlin <hello@pierrelouislegrand.com>
2022-08-13 17:28:01 +0200

    Style: Order imports

--- src/Backend/MotorsportTracker/Racer/Infrastructure/Persistence/Doctrine/Repository/RacerGatewayUsingDoctrine.php
+++ src/Backend/MotorsportTracker/Racer/Infrastructure/Persistence/Doctrine/Repository/RacerGatewayUsingDoctrine.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Kishlin\Backend\MotorsportTracker\Racer\Infrastructure\Persistence\Doctrine\Repository;
 
-use Kishlin\Backend\MotorsportTracker\Racer\Domain\Gateway\RacerGateway;
 use Kishlin\Backend\MotorsportTracker\Racer\Domain\Entity\Racer;
+use Kishlin\Backend\MotorsportTracker\Racer\Domain\Gateway\RacerGateway;
 use Kishlin\Backend\Shared\Infrastructure\Persistence\Doctrine\Repository\DoctrineRepository;
 
 final class RacerGatewayUsingDoctrine extends DoctrineRepository implements RacerGateway
