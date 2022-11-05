1d418b18311e63c7062ecaf4b338aaa9dee1b222
Kishlin <hello@pierrelouislegrand.com>
2022-11-05 23:30:44 +0100

    Hotfix: return the value object class instead of self

--- src/Backend/MotorsportTracker/Standing/Infrastructure/Persistence/Doctrine/DbalTypes/TeamStandingPointsType.php
+++ src/Backend/MotorsportTracker/Standing/Infrastructure/Persistence/Doctrine/DbalTypes/TeamStandingPointsType.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace Kishlin\Backend\MotorsportTracker\Standing\Infrastructure\Persistence\Doctrine\DbalTypes;
 
+use Kishlin\Backend\MotorsportTracker\Standing\Domain\ValueObject\TeamStandingPoints;
 use Kishlin\Backend\Shared\Infrastructure\Persistence\Doctrine\DbalTypes\AbstractFloatType;
 
 final class TeamStandingPointsType extends AbstractFloatType
 {
     protected function mappedClass(): string
     {
-        return TeamStandingPointsType::class;
+        return TeamStandingPoints::class;
     }
 }
