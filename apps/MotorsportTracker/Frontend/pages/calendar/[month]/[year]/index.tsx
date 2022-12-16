02a491e3d9c688f4eacafa5f0188d657ef2d2ff0
Kishlin <hello@pierrelouislegrand.com>
2022-12-16 18:24:29 +0100

    Feature: Fetch events on calendar page load

--- apps/MotorsportTracker/Frontend/pages/calendar/[month]/[year]/index.tsx
+++ apps/MotorsportTracker/Frontend/pages/calendar/[month]/[year]/index.tsx
@@ -1,19 +1,28 @@
 // @ts-ignore
 import React from 'react';
-import { useRouter } from 'next/router';
 
 import Calendar from '../../../../src/Calendar/Component/Calendar';
 import Layout from '../../../../src/Shared/Ui/Layout/Layout';
 
-const CalendarPage: React.FunctionComponent = () => {
-    const router = useRouter();
-    const { month, year } = router.query;
+import listOfAllMonths from '../../../../src/Calendar/Utils/Date/listOfAllMonths';
 
-    if (undefined === month || undefined === year) {
-        return <noscript />;
-    }
+declare type CalendarPathParams = {
+    params: {
+        month: string,
+        year: string,
+    },
+};
 
+declare type CalendarProps = {
+    month: string,
+    year: string,
+    events: {},
+}
+
+const CalendarPage: React.FunctionComponent<CalendarProps> = ({ events, year, month }) => {
     const date = new Date(Date.parse(`${month} 1, ${year}`));
+
+    console.log(events);
 
     return (
         <Layout>
@@ -21,5 +30,24 @@ const CalendarPage: React.FunctionComponent = () => {
         </Layout>
     );
 };
+
+export const getStaticProps = async ({ params: { month, year } }: CalendarPathParams) => {
+    const props: CalendarProps = { year, month, events: {} };
+
+    const response = await fetch(`http://backend:8000/api/v1/events/calendar/${month}/${year}`);
+    props.events = await response.json();
+
+    return { props };
+};
+
+export async function getStaticPaths(): Promise<{ paths: Array<CalendarPathParams>, fallback: boolean }> {
+    const paths: Array<CalendarPathParams> = [];
+
+    for (let year = 2022; 2023 >= year; year += 1) {
+        paths.push(...listOfAllMonths().map((month: string) => ({ params: { year: year.toString(), month } })));
+    }
+
+    return { paths, fallback: false };
+}
 
 export default CalendarPage;
---
+++ apps/MotorsportTracker/Frontend/src/Calendar/Utils/Date/listOfAllMonths.ts
@@ -0,0 +1,33 @@
+declare type ListOfAllMonths = () => [
+    'january',
+    'february',
+    'march',
+    'april',
+    'may',
+    'june',
+    'july',
+    'august',
+    'september',
+    'october',
+    'november',
+    'december',
+];
+
+const listOfAllMonths: ListOfAllMonths = () => (
+    [
+        'january',
+        'february',
+        'march',
+        'april',
+        'may',
+        'june',
+        'july',
+        'august',
+        'september',
+        'october',
+        'november',
+        'december',
+    ]
+);
+
+export default listOfAllMonths;
