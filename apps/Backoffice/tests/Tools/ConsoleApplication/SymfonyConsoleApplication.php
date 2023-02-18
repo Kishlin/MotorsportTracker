aebac8a52ada9a8ebc957d7d313bf3ca3acc0985
Kishlin <hello@pierrelouislegrand.com>
2023-02-18 04:56:19 +0100

    Feature: Allow time modifications in acceptance tests

--- apps/Backoffice/tests/Tools/ConsoleApplication/SymfonyConsoleApplication.php
+++ apps/Backoffice/tests/Tools/ConsoleApplication/SymfonyConsoleApplication.php
@@ -4,8 +4,19 @@ declare(strict_types=1);
 
 namespace Kishlin\Tests\Apps\Backoffice\Tools\ConsoleApplication;
 
+use DateInterval;
+use Kishlin\Backend\Shared\Domain\Time\Clock;
+use Kishlin\Backend\Shared\Infrastructure\Time\FrozenClock;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 
 final class SymfonyConsoleApplication extends Application implements ConsoleApplicationInterface
 {
+    public function advanceInTime(DateInterval $dateInterval): void
+    {
+        $clock = $this->getKernel()->getContainer()->get(Clock::class);
+
+        assert($clock instanceof FrozenClock); // @phpstan-ignore-line
+
+        $clock->set($clock->now()->add($dateInterval));
+    }
 }
