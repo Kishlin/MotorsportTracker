5acee2cf047e699742b0feef743a01de9edd575b
Kishlin <hello@pierrelouislegrand.com>
2023-02-20 17:09:16 +0000

    Hotfix: Correctly handle days when computing schedule key

--- apps/MotorsportTracker/Frontend/src/MotorsportTracker/Schedule/Utils/Date/formatDateAsScheduleKey.ts
+++ apps/MotorsportTracker/Frontend/src/MotorsportTracker/Schedule/Utils/Date/formatDateAsScheduleKey.ts
@@ -3,7 +3,7 @@ declare type FormatDateAsScheduleKey = (date: Date) => `${string}-${string}-${st
 const formatDateAsScheduleKey: FormatDateAsScheduleKey = (date) => {
     const year = date.getUTCFullYear();
     const month = (`00${date.getUTCMonth() + 1}`).slice(-2);
-    const day = (`00${date.getUTCDate() + 1}`).slice(-2);
+    const day = (`00${date.getUTCDate()}`).slice(-2);
 
     return `${year}-${month}-${day}`;
 };
