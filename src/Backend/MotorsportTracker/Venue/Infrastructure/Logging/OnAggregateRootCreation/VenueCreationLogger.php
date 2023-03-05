a794427d0c9ea7ca287d6952049d456864ccb9df
Kishlin <hello@pierrelouislegrand.com>
2023-03-05 14:05:50 +0100

    Hotfix: Subscribe to the correct domain event

--- src/Backend/MotorsportTracker/Venue/Infrastructure/Logging/OnAggregateRootCreation/VenueCreationLogger.php
+++ src/Backend/MotorsportTracker/Venue/Infrastructure/Logging/OnAggregateRootCreation/VenueCreationLogger.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Kishlin\Backend\MotorsportTracker\Venue\Infrastructure\Logging\OnAggregateRootCreation;
 
-use Kishlin\Backend\MotorsportTracker\Event\Domain\DomainEvent\EventSessionCreatedDomainEvent;
+use Kishlin\Backend\MotorsportTracker\Venue\Domain\DomainEvent\VenueCreatedDomainEvent;
 use Kishlin\Backend\Shared\Infrastructure\Logging\OnAggregateRootCreation\AggregateRootCreationLogger;
 
 final class VenueCreationLogger extends AggregateRootCreationLogger
 {
-    public function __invoke(EventSessionCreatedDomainEvent $event): void
+    public function __invoke(VenueCreatedDomainEvent $event): void
     {
         $this->logCreation(
             'venues',
