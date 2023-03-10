8b44becb6e25e093df5f6eea54ae4e0f585fd125
Kishlin <hello@pierrelouislegrand.com>
2023-03-10 18:10:56 +0100

    Feature: Fix Doctrine Migrations issues with PostgreSQL

--- apps/MotorsportTracker/Backend/cli-config.php
+++ apps/MotorsportTracker/Backend/cli-config.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 use Doctrine\Migrations\Configuration\EntityManager\ExistingEntityManager;
 use Doctrine\Migrations\Configuration\Migration\PhpFile;
 use Doctrine\Migrations\DependencyFactory;
+use Doctrine\ORM\Tools\ToolEvents;
 use Kishlin\Backend\MotorsportTracker\Shared\Infrastructure\Persistence\Doctrine\EntityManagerFactory\MotorsportTrackerEntityManagerFactory;
+use Kishlin\Backend\Shared\Infrastructure\Persistence\Doctrine\MigrationFix\FixPostgresqlDefaultSchemaListener;
 use Symfony\Component\Dotenv\Dotenv;
 
 require __DIR__ . '/../../../vendor/autoload.php';
@@ -27,4 +29,8 @@ $entityManager = MotorsportTrackerEntityManagerFactory::create(
     environment: 'dev',
 );
 
-return DependencyFactory::fromEntityManager($config, new ExistingEntityManager($entityManager));
+$em = DependencyFactory::fromEntityManager($config, new ExistingEntityManager($entityManager));
+
+$em->getConnection()->getEventManager()->addEventListener(ToolEvents::postGenerateSchema, new FixPostgresqlDefaultSchemaListener());
+
+return $em;
