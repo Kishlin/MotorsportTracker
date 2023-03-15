a8d81e6147377bab65306a9e113d94a9b5c8dd41
Kishlin <hello@pierrelouislegrand.com>
2023-03-15 20:25:37 +0100

    Feature: Use the homemade connection instead of Doctrine

--- src/Backend/Shared/Infrastructure/Logging/OnAggregateRootCreation/AggregateRootCreationLogger.php
+++ src/Backend/Shared/Infrastructure/Logging/OnAggregateRootCreation/AggregateRootCreationLogger.php
@@ -4,28 +4,31 @@ declare(strict_types=1);
 
 namespace Kishlin\Backend\Shared\Infrastructure\Logging\OnAggregateRootCreation;
 
-use Doctrine\DBAL\Exception;
-use Doctrine\ORM\EntityManagerInterface;
+use Kishlin\Backend\Persistence\Core\Connection\Connection;
+use Kishlin\Backend\Persistence\SQL\SQLQuery;
 use Kishlin\Backend\Shared\Domain\Bus\Event\EventSubscriber;
 use Psr\Log\LoggerInterface;
+use Throwable;
 
 abstract class AggregateRootCreationLogger implements EventSubscriber
 {
     public function __construct(
-        private readonly EntityManagerInterface $em,
+        private readonly Connection $connection,
         private readonly LoggerInterface $logger,
     ) {
     }
 
     protected function logCreation(string $table, string $id, string $title): void
     {
+        /** @noinspection SqlResolve */
+        $query = SQLQuery::create("SELECT * FROM {$table} WHERE id = :id;", ['id' => $id]);
+
         try {
-            /** @noinspection SqlResolve */
-            $data = $this->em->getConnection()->fetchAssociative("SELECT * FROM {$table} WHERE id = :id", ['id' => $id]);
+            $data = $this->connection->execute($query)->fetchAllAssociative();
             assert(is_array($data));
 
             $this->logger->info("Saved {$title}.", $data);
-        } catch (Exception) {
+        } catch (Throwable) {
             $this->logger->warning("Unable to log creation of {$title} [{$id}].");
         }
     }
