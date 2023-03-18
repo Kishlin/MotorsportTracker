c7dedb6b7d9516175f15a4d283a388c3edd7c81c
Kishlin <hello@pierrelouislegrand.com>
2023-03-18 06:24:34 +0100

    Tests: Test StringHelper keeps underscores when computing slugs

--- tests/Backend/IsolatedTests/Tools/Helpers/StringHelperTest.php
+++ tests/Backend/IsolatedTests/Tools/Helpers/StringHelperTest.php
@@ -7,6 +7,10 @@ namespace Kishlin\Tests\Backend\IsolatedTests\Tools\Helpers;
 use Kishlin\Backend\Tools\Helpers\StringHelper;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @internal
+ * @coversNothing
+ */
 final class StringHelperTest extends TestCase
 {
     public function testItCanSlugify(): void
@@ -14,5 +18,10 @@ final class StringHelperTest extends TestCase
         self::assertSame('formula-one', StringHelper::slugify('Formula One'));
         self::assertSame('formula-one_dutch-grand-prix', StringHelper::slugify('Formula One', 'Dutch Grand Prix'));
         self::assertSame('formula-one_dutch-grand-prix_race', StringHelper::slugify('Formula One', 'Dutch Grand Prix', 'Race'));
+    }
+
+    public function testItKeepsUnderscores(): void
+    {
+        self::assertSame('test_underscore', StringHelper::slugify('test_underscore'));
     }
 }
