9360e78d40965236d80a6598ec7fcbe8923f825f
Kishlin <hello@pierrelouislegrand.com>
2023-03-19 20:16:36 +0100

    Feature: play with fastf1 to see how it works

---
+++ playground/fastf1/tests/test_events.py
@@ -0,0 +1,191 @@
+import datetime
+
+import pandas as pd
+import pytest
+
+import fastf1.core
+import fastf1.events
+
+
+@pytest.mark.parametrize("gp", ['Bahrain', 'Bharain', 'Sakhir', 1])
+@pytest.mark.parametrize("identifier", ['Q', 4, 'Qualifying'])
+def test_get_session(gp, identifier):
+    session = fastf1.get_session(2021, gp, identifier)
+    assert session.event['EventName'] == 'Bahrain Grand Prix'
+    assert session.name == 'Qualifying'
+
+
+@pytest.mark.parametrize("test_n, pass_1", [(0, False), (1, True), (2, False)])
+@pytest.mark.parametrize(
+    "session_n, pass_2",
+    [(0, False), (1, True), (2, True), (3, True), (4, False)]
+)
+def test_get_testing_session(test_n, session_n, pass_1, pass_2):
+    if pass_1 and pass_2:
+        session = fastf1.get_testing_session(2021, test_n, session_n)
+        assert isinstance(session, fastf1.core.Session)
+        assert session.name == f"Practice {session_n}"
+    else:
+        with pytest.raises(ValueError):
+            fastf1.get_testing_session(2021, test_n, session_n)
+
+
+@pytest.mark.parametrize("dt", [datetime.datetime(year=2022, month=6, day=1)])
+def test_get_events_remaining(dt):
+    events = fastf1.get_events_remaining(dt)
+    assert len(events) == 15
+
+
+@pytest.mark.parametrize("dt",
+                         [datetime.datetime(year=2022, month=12, day=31)])
+def test_events_remaining_after_season(dt):
+    events = fastf1.get_events_remaining(dt)
+    assert len(events) == 0
+
+
+@pytest.mark.parametrize("dt", [datetime.datetime(year=2022, month=1, day=1)])
+def test_events_remaining_before_season(dt):
+    events = fastf1.get_events_remaining(dt)
+    assert len(events) == 24
+
+
+@pytest.mark.parametrize("gp", ['Bahrain', 'Bharain', 'Sakhir', 1])
+def test_get_event(gp):
+    event = fastf1.get_event(2021, gp)
+    assert event.EventName == 'Bahrain Grand Prix'
+
+
+def test_get_event_round_zero():
+    with pytest.raises(ValueError, match="testing event by round number"):
+        fastf1.get_event(2021, 0)
+
+
+def test_get_testing_event():
+    # 0 is not a valid number for a testing event
+    with pytest.raises(ValueError):
+        fastf1.get_testing_event(2021, 0)
+
+    session = fastf1.get_testing_event(2021, 1)
+    assert isinstance(session, fastf1.events.Event)
+
+    # only one testing event in 2021
+    with pytest.raises(ValueError):
+        fastf1.get_testing_event(2021, 2)
+
+
+def test_event_schedule_partial_data_init():
+    schedule = fastf1.events.EventSchedule(
+        {'EventName': ['A', 'B', 'C'], 'Session1Date': [None, None, None]}
+    )
+    assert schedule.dtypes['EventName'] == 'object'
+    assert schedule.dtypes['Session1Date'] == '<M8[ns]'
+
+
+def test_event_schedule_constructor_sliced():
+    schedule = fastf1.events.EventSchedule({'EventName': ['A', 'B', 'C']},
+                                           year=2020)
+    event = schedule.iloc[0]
+    assert isinstance(event, fastf1.events.Event)
+    assert event.year == 2020
+
+
+def test_event_schedule_is_testing():
+    schedule = fastf1.events.EventSchedule(
+        {'EventFormat': ['conventional', 'testing']}
+    )
+    assert (schedule.is_testing() == [False, True]).all()
+
+
+def test_event_schedule_get_event_by_round_number():
+    schedule = fastf1.events.EventSchedule(
+        {'EventName': ['T1', 'A', 'B', 'C', 'D'],
+         'RoundNumber': [0, 1, 2, 3, 4]}
+    )
+    assert schedule.get_event_by_round(2).EventName == 'B'
+
+    with pytest.raises(ValueError, match="testing event by round number"):
+        schedule.get_event_by_round(0)
+
+    with pytest.raises(ValueError, match="Invalid round"):
+        schedule.get_event_by_round(10)
+
+
+def test_event_schedule_get_by_name():
+    schedule = fastf1.events.EventSchedule(
+        {
+            'EventName': [
+                'testA',
+                'TESTB',
+                'test_test'
+            ]
+        }
+    )
+
+    assert schedule.get_event_by_name('testA').EventName == 'testA'
+    assert schedule.get_event_by_name('TESTA').EventName == 'testA'
+    assert schedule.get_event_by_name('testb').EventName == 'TESTB'
+    assert schedule.get_event_by_name('test-test').EventName == 'test_test'
+
+
+def test_event_is_testing():
+    assert fastf1.get_testing_event(2021, 1).is_testing()
+    assert not fastf1.get_event(2021, 1).is_testing()
+
+
+def test_event_get_session_name():
+    event = fastf1.get_event(2021, 1)
+    assert event.get_session_name(3) == 'Practice 3'
+    assert event.get_session_name('Q') == 'Qualifying'
+    assert event.get_session_name('praCtice 1') == 'Practice 1'
+
+    # sprint qualifying name peculiarities
+    event = fastf1.get_event(2021, 14)
+    assert event.year == 2021
+    assert event.get_session_name('SQ') == 'Sprint Qualifying'
+    assert event.get_session_name('S') == 'Sprint Qualifying'
+    assert event.get_session_name('Sprint') == 'Sprint Qualifying'
+    assert event.get_session_name('Sprint Qualifying') == 'Sprint Qualifying'
+
+    event = fastf1.get_event(2022, 4)
+    assert event.year == 2022
+    assert event.get_session_name('SQ') == 'Sprint'
+    assert event.get_session_name('S') == 'Sprint'
+    assert event.get_session_name('Sprint') == 'Sprint'
+    assert event.get_session_name('Sprint Qualifying') == 'Sprint'
+
+
+def test_event_get_session_date():
+    event = fastf1.get_event(2021, 1)
+    sd = event.get_session_date('Q')
+    assert sd == event.Session4Date
+    assert isinstance(sd, pd.Timestamp)
+
+
+@pytest.mark.parametrize(
+    "meth_name,args,expected_name",
+    [
+        ['get_session', ['qualifying'], 'Qualifying'],
+        ['get_session', ['R'], 'Race'],
+        ['get_session', [1], 'Practice 1'],
+        ['get_race', [], 'Race'],
+        ['get_qualifying', [], 'Qualifying'],
+        ['get_sprint', [], 'Sprint Qualifying'],
+        ['get_practice', [1], 'Practice 1'],
+        ['get_practice', [2], 'Practice 2'],
+    ]
+)
+def test_event_get_session(meth_name, args, expected_name):
+    event = fastf1.get_event(2021, 14)
+    session = getattr(event, meth_name)(*args)
+    assert session.name == expected_name
+
+
+def test_event_get_nonexistent_session():
+    with pytest.raises(ValueError, match="does not exist"):
+        fastf1.get_session(2020, 13, 'FP2')
+
+
+def test_event_get_nonexistent_session_date():
+    event = fastf1.get_event(2020, 13)
+    with pytest.raises(ValueError, match="does not exist"):
+        event.get_session_date('FP2')
---
+++ playground/fastf1/tests/test_input_data_handling.py
@@ -0,0 +1,130 @@
+# test some known special cases
+
+import pytest
+
+import logging
+
+import pandas as pd
+
+import fastf1
+import fastf1.ergast
+import fastf1.testing
+from fastf1.testing.reference_values import LAP_DTYPES
+
+
+@pytest.mark.f1telapi
+def test_ergast_lookup_fail():
+    fastf1.testing.run_in_subprocess(_test_ergast_lookup_fail)
+
+
+def _test_ergast_lookup_fail():
+    fastf1.Cache.enable_cache('test_cache')
+    log_handle = fastf1.testing.capture_log()
+
+    # ergast lookup fails if data is requested to soon after a session ends
+
+    def fail_load(*args, **kwargs):
+        raise Exception
+    fastf1.ergast.fetch_results = fail_load  # force function call to fail
+
+    session = fastf1.get_session(2020, 3, 'FP2')  # rainy and short session, good for fast test/quick loading
+    session.load(telemetry=False, weather=False)
+
+    assert "Failed to load data from Ergast API!" in log_handle.text  # the warning
+    assert "Finished loading data" in log_handle.text  # indicates success
+
+
+@pytest.mark.f1telapi
+def test_crash_lap_added_1():
+    # sainz crashed in his 14th lap, there need to be all 14 laps
+    session = fastf1.get_session(2021, "Monza", 'FP2')
+
+    session.load(telemetry=False)
+    assert session.laps.pick_driver('SAI').shape[0] == 14
+
+
+@pytest.mark.f1telapi
+def test_crash_lap_added_2():
+    # verstappen crashed on his first lap, the lap needs to exist
+    session = fastf1.get_session(2021, 'British Grand Prix', 'R')
+
+    session.load(telemetry=False)
+    assert session.laps.pick_driver('VER').shape[0] == 1
+
+
+@pytest.mark.f1telapi
+def test_no_extra_lap_if_race_not_started():
+    # tsunoda had a technical issue shortly before the race and could not
+    # start even though he is listed in the drivers list
+    session = fastf1.get_session(2022, 2, 'R')
+
+    session.load(telemetry=False, weather=False)
+    assert session.laps.size
+    assert session.laps.pick_driver('TSU').size == 0
+
+
+@pytest.mark.f1telapi
+def test_no_timing_app_data():
+    fastf1.testing.run_in_subprocess(_test_no_timing_app_data)
+
+
+def _test_no_timing_app_data():
+    # subprocess test because api parser function is overwritten
+    log_handle = fastf1.testing.capture_log(logging.WARNING)
+
+    def _mock(*args, **kwargs):
+        return pd.DataFrame(
+            {'LapNumber': [], 'Driver': [], 'LapTime': [], 'Stint': [],
+             'TotalLaps': [], 'Compound': [], 'New': [],
+             'TyresNotChanged': [], 'Time': [], 'LapFlags': [],
+             'LapCountTime': [], 'StartLaps': [], 'Outlap': []}
+        )
+
+    fastf1.api.timing_app_data = _mock
+
+    session = fastf1.get_session(2020, 'Italy', 'R')
+    with fastf1.Cache.disabled():
+        session.load(telemetry=False, weather=False)
+
+    assert 'Failed to load lap data!' not in log_handle.text
+    assert 'No tyre data for driver' in log_handle.text
+
+    assert session.laps.size
+    assert all([col in session.laps.columns for col in LAP_DTYPES.keys()])
+
+
+@pytest.mark.f1telapi
+def test_inlap_added():
+    session = fastf1.get_session(2021, 'Mexico City', 'Q')
+
+    with fastf1.Cache.disabled():
+        session.load(telemetry=False)
+
+    last = session.laps.pick_driver('PER').iloc[-1]
+    assert not pd.isnull(last['PitInTime'])
+    assert not pd.isnull(last['Time'])
+
+
+@pytest.mark.f1telapi
+def test_lap_start_time_after_red_flag():
+    # see GH#167
+    session = fastf1.get_session(2022, 'Saudi Arabia', 'Q')
+    session.load(telemetry=False, weather=False, messages=False)
+
+    restart_time = pd.to_timedelta('01:54:24.197000')
+
+    # ensure that verstappens first lap after the restart was also started
+    # after the restart
+    ver_laps = session.laps.pick_driver('VER')
+    idx = ver_laps[(ver_laps['Time'] > restart_time)
+                   & pd.notna(ver_laps['Time'])].index[0]
+    assert ver_laps.loc[idx]['LapStartTime'] > restart_time
+
+
+@pytest.mark.f1telapi
+def test_partial_lap_retired_added():
+    # test that a last (partial) lap is added for drivers that retire on track
+    session = fastf1.get_session(2022, 1, 'R')
+    session.load()
+
+    assert session.laps.pick_driver('11').iloc[-1]['FastF1Generated']
