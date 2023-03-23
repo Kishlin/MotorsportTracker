ae34b81d373b91693dd3314605d90274acb815e3
Kishlin <hello@pierrelouislegrand.com>
2023-03-23 22:30:53 +0100

    Hotfix: Listen to the correct event

--- src/Backend/MotorsportTracker/Result/Infrastructure/Logging/OnAggregateRootCreation/RaceLapCreationLogger.php
+++ src/Backend/MotorsportTracker/Result/Infrastructure/Logging/OnAggregateRootCreation/RaceLapCreationLogger.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Kishlin\Backend\MotorsportTracker\Result\Infrastructure\Logging\OnAggregateRootCreation;
 
-use Kishlin\Backend\MotorsportTracker\Result\Domain\DomainEvent\EntryCreatedDomainEvent;
+use Kishlin\Backend\MotorsportTracker\Result\Domain\DomainEvent\RaceLapCreatedDomainEvent;
 use Kishlin\Backend\Shared\Infrastructure\Logging\OnAggregateRootCreation\AggregateRootCreationLogger;
 
 final class RaceLapCreationLogger extends AggregateRootCreationLogger
 {
-    public function __invoke(EntryCreatedDomainEvent $event): void
+    public function __invoke(RaceLapCreatedDomainEvent $event): void
     {
         $this->logCreation(
             'race_lap',
