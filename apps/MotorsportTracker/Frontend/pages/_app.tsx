0c3bfdec03a35acd15eedfe712f689f073dbf0e4
Kishlin <hello@pierrelouislegrand.com>
2023-03-25 15:56:20 +0100

    Feature: Use a styled scrollbar

--- apps/MotorsportTracker/Frontend/pages/_app.tsx
+++ apps/MotorsportTracker/Frontend/pages/_app.tsx
@@ -25,6 +25,37 @@ const paletteOptions: ThemeOptions = {
         },
         divider: 'rgba(255,255,255,0.12)',
     },
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: {
+                body: {
+                    scrollbarColor: '#6b6b6b #2b2b2b',
+                    '&::-webkit-scrollbar, & *::-webkit-scrollbar': {
+                        minWidth: 15,
+                        backgroundColor: '#2b2b2b',
+                    },
+                    '&::-webkit-scrollbar-thumb, & *::-webkit-scrollbar-thumb': {
+                        borderRadius: 8,
+                        backgroundColor: '#6b6b6b',
+                        minHeight: 24,
+                        border: '3px solid #2b2b2b',
+                    },
+                    '&::-webkit-scrollbar-thumb:focus, & *::-webkit-scrollbar-thumb:focus': {
+                        backgroundColor: '#959595',
+                    },
+                    '&::-webkit-scrollbar-thumb:active, & *::-webkit-scrollbar-thumb:active': {
+                        backgroundColor: '#959595',
+                    },
+                    '&::-webkit-scrollbar-thumb:hover, & *::-webkit-scrollbar-thumb:hover': {
+                        backgroundColor: '#959595',
+                    },
+                    '&::-webkit-scrollbar-corner, & *::-webkit-scrollbar-corner': {
+                        backgroundColor: '#2b2b2b',
+                    },
+                },
+            },
+        },
+    },
 };
 
 const darkTheme = createTheme(paletteOptions);
