c2b067f241a387da0b8871ee770e825b8647f59b
Kishlin <hello@pierrelouislegrand.com>
2023-04-13 20:12:10 +0200

    Configuration: Add Monolog to monitor logs

--- apps/MotorsportTracker/Backend/config/bundles.php
+++ apps/MotorsportTracker/Backend/config/bundles.php
@@ -2,5 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class             => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class     => ['all' => true],
 ];
