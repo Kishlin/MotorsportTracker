6be9da25e3231d034211a7bdbbfc18ea5db9f9fc
Kishlin <hello@pierrelouislegrand.com>
2023-07-15 18:16:19 +0200

    Hotfix: Listen to the correct event

--- src/Backend/MotorsportTracker/Team/Infrastructure/Logging/OnAggregateRootCreation/TeamCreationLogger.php
+++ src/Backend/MotorsportTracker/Team/Infrastructure/Logging/OnAggregateRootCreation/TeamCreationLogger.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Kishlin\Backend\MotorsportTracker\Team\Infrastructure\Logging\OnAggregateRootCreation;
 
-use Kishlin\Backend\MotorsportTracker\Driver\Domain\DomainEvent\DriverCreatedDomainEvent;
+use Kishlin\Backend\MotorsportTracker\Team\Domain\DomainEvent\TeamCreatedDomainEvent;
 use Kishlin\Backend\Shared\Infrastructure\Logging\OnAggregateRootCreation\AggregateRootCreationLogger;
 
 final class TeamCreationLogger extends AggregateRootCreationLogger
 {
-    public function __invoke(DriverCreatedDomainEvent $event): void
+    public function __invoke(TeamCreatedDomainEvent $event): void
     {
         $this->logCreation(
             'team',
