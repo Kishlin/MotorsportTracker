76f390a6fb77ca3b348d6b9f02850fa3e8c2a5e2
Kishlin <hello@pierrelouislegrand.com>
2023-07-26 02:00:47 +0200

    Feature: Add missing slugs to handle all series

--- src/Backend/MotorsportTracker/Championship/Domain/Helper/ChampionshipSlugHelper.php
+++ src/Backend/MotorsportTracker/Championship/Domain/Helper/ChampionshipSlugHelper.php
@@ -17,9 +17,30 @@ final class ChampionshipSlugHelper
     public static function unslugify(string $slug): string
     {
         return match ($slug) {
+            'formula-one' => 'Formula One',
+
+            'fia-formula-2-championship' => 'FIA Formula 2 Championship',
+            'fia-formula-3-championship' => 'FIA Formula 3 Championship',
+
+            'formula-e' => 'Formula E',
+
+            'f4-france' => 'F4 France',
+
+            'w-series' => 'W Series',
+
             'world-endurance-championship' => 'World Endurance Championship',
 
-            'formula-one' => 'Formula One',
+            'gt-world-challenge-europe' => 'GT World Challenge Europe',
+
+            'gt-world-challenge-europe-sprint-cup' => 'GT World Challenge Europe Sprint Cup',
+
+            'gt4-france' => 'GT4 France',
+
+            'imsa-sportscar-championship' => 'IMSA SportsCar Championship',
+
+            'adac-gt-masters' => 'ADAC GT Masters',
+
+            'motogp' => 'MotoGP',
 
             default => throw new UnhandledMatchError("Cannot find a match for {$slug}."),
         };
