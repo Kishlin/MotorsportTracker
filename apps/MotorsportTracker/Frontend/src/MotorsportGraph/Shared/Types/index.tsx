79ff493e5a1046329da29147a98f132ee3da1413
Kishlin <hello@pierrelouislegrand.com>
2023-08-13 20:01:37 +0200

    Project: Add Types for the new graphs

--- apps/MotorsportTracker/Frontend/src/MotorsportGraph/Shared/Types/index.tsx
+++ apps/MotorsportTracker/Frontend/src/MotorsportGraph/Shared/Types/index.tsx
@@ -1,25 +1,65 @@
-export type LapByLapSeries = {
+export interface Series {
     color: string,
     index: number,
     car_number: number,
     short_code: string,
-    lapTimes: {
-        [key: string]: number,
-    },
-};
+}
 
-export type LapByLapGraphData = {
+export interface GraphData<T extends Series> {
     session: {
         id: string,
         type: string,
     },
+    series: Array<T>,
+}
+
+export interface LapByLapSeries extends Series {
+    lapTimes: {
+        [key: string]: number,
+    },
+}
+
+export interface TyreHistorySeries extends Series {
+    tyre_history: {
+        [key: number]: {
+            laps: number,
+            type: 'H'|'M'|'S'|'I'|'W',
+            wear: 'n'|'u',
+        }
+    }
+}
+
+export interface PositionChangeSeries extends Series {
+    changes: number,
+    grid: number,
+    finish: number,
+}
+
+export interface FastestLapSeries extends Series {
+    fastest: string,
+    delta: number,
+}
+
+export interface LapByLapGraphData extends GraphData<LapByLapSeries> {
     laps: number,
     lapTimes: {
         fastest: number,
         slowest: number,
     },
-    series: Array<LapByLapSeries>,
-};
+}
+
+export interface TyreHistoryGraphData extends GraphData<TyreHistorySeries> {
+    laps: number,
+}
+
+export interface PositionChangeGraphData extends GraphData<PositionChangeSeries> {
+    minChanges: number,
+    maxChanges: number,
+}
+
+export interface FastestLapGraphData extends GraphData<FastestLapSeries> {
+    maxDelta: number,
+}
 
 export type EventGraphList<T> = {
     [key: string]: T,
