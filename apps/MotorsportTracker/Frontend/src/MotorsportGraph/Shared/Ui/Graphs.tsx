3afe3751322a36d28256518f0ab920daceaa076b
Kishlin <hello@pierrelouislegrand.com>
2023-08-14 13:13:39 +0200

    Hotfix: Handle events missing lap by lap graphs

--- apps/MotorsportTracker/Frontend/src/MotorsportGraph/Shared/Ui/Graphs.tsx
+++ apps/MotorsportTracker/Frontend/src/MotorsportGraph/Shared/Ui/Graphs.tsx
@@ -16,11 +16,14 @@ const Graphs: React.FunctionComponent<GraphsProps> = ({ graphs, isMultiDriver })
         return <Typography align="center">There are no graphs available at this time.</Typography>;
     }
 
-    const lapByLapGraphsJSX = Object.keys(graphs['lap-by-lap-pace']).map((key: string) => (
-        <LapByLapGraph key={key} isMultiDriver={isMultiDriver} data={graphs['lap-by-lap-pace'][key]} />
-    ));
+    let lapByLapGraphsJSX: React.ReactElement[] = [];
+    if (undefined !== graphs['lap-by-lap-pace']) {
+        lapByLapGraphsJSX = Object.keys(graphs['lap-by-lap-pace']).map((key: string) => (
+            <LapByLapGraph key={key} isMultiDriver={isMultiDriver} data={graphs['lap-by-lap-pace'][key]} />
+        ));
+    }
 
-    let tyreHistoryGraphsJSX: JSX.Element[] = [];
+    let tyreHistoryGraphsJSX: React.ReactElement[] = [];
     if (undefined !== graphs['tyre-history']) {
         tyreHistoryGraphsJSX = Object.keys(graphs['tyre-history']).map((key: string) => (
             <TyreHistoryGraph key={key} data={graphs['tyre-history'][key]} />
