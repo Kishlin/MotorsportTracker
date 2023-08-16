ab2f9f6e3172d6f3add0a60604ac8fd4569be34c
Kishlin <hello@pierrelouislegrand.com>
2023-08-16 04:20:34 +0200

    Feature: Cache some APIs results for performance

--- apps/MotorsportTracker/Frontend/src/MotorsportTracker/Event/Api/SeasonApi.ts
+++ apps/MotorsportTracker/Frontend/src/MotorsportTracker/Event/Api/SeasonApi.ts
@@ -1,11 +1,24 @@
+import cacheData from 'memory-cache';
+
 import { SeasonEvents } from '../../Shared/Types';
 
 export type SeasonApi = (championshipSlug: string, year: number) => Promise<SeasonEvents>;
 
 const seasonApi: SeasonApi = async (championshipSlug, year) => {
+    const key = `season-${championshipSlug}-${year}`;
+
+    const cached = cacheData.get(key);
+    if (cached) {
+        return cached;
+    }
+
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/events/${championshipSlug}/${year}`);
 
-    return await response.json() as SeasonEvents;
+    const value = (await response.json() as SeasonEvents);
+
+    cacheData.put(key, value, 3600000);
+
+    return value;
 };
 
 export default seasonApi;
--- apps/MotorsportTracker/Frontend/src/MotorsportTracker/Standing/Api/AvailableStandingsApi.ts
+++ apps/MotorsportTracker/Frontend/src/MotorsportTracker/Standing/Api/AvailableStandingsApi.ts
@@ -1,11 +1,24 @@
+import cacheData from 'memory-cache';
+
 import { AvailableStandings } from '../../Shared/Types';
 
 export type AvailableStandingApi = (championship: string, year: string) => Promise<AvailableStandings>;
 
 const availableStandingApi: AvailableStandingApi = async (championship, year) => {
+    const key = `standings-options-${championship}-${year}`;
+
+    const cached = cacheData.get(key);
+    if (cached) {
+        return cached;
+    }
+
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/standings/options/${championship}/${year}`);
 
-    return await response.json() as AvailableStandings;
+    const value = await response.json() as AvailableStandings;
+
+    cacheData.put(key, value, 3600000);
+
+    return value;
 };
 
 export default availableStandingApi;
