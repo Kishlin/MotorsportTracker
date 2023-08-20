1aea5ac86553bab065bdf1dee30310ead0e8afaa
Kishlin <hello@pierrelouislegrand.com>
2023-08-20 16:14:46 +0200

    Feature: Add the possibility to compute results for a season or event

---
+++ apps/Backoffice/src/MotorsportCache/Result/SyncResultsForEventListCommandUsingSymfony.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kishlin\Apps\Backoffice\MotorsportCache\Result;
+
+use Kishlin\Backend\MotorsportCache\Result\Application\UpdateEventResultsCache\UpdateEventResultsCacheCommand;
+use Kishlin\Backend\MotorsportTracker\Event\Application\GetSeasonEventIdList\GetSeasonEventIdListQuery;
+use Kishlin\Backend\MotorsportTracker\Event\Application\GetSeasonEventIdList\GetSeasonEventIdListResponse;
+use Kishlin\Backend\Shared\Domain\Bus\Command\CommandBus;
+use Kishlin\Backend\Shared\Domain\Bus\Query\QueryBus;
+use Kishlin\Backend\Tools\Infrastructure\Symfony\Command\SymfonyCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Throwable;
+
+final class SyncResultsForEventListCommandUsingSymfony extends SymfonyCommand
+{
+    public const NAME = 'kishlin:motorsport-cache:results:compute';
+
+    private const ARGUMENT_CHAMPIONSHIP = 'championship';
+    private const QUESTION_CHAMPIONSHIP = "Please enter the name of the championship:\n";
+
+    private const ARGUMENT_YEAR = 'year';
+    private const QUESTION_YEAR = "Please enter the year of the season:\n";
+
+    private const OPTION_EVENT = 'event';
+
+    public function __construct(
+        private readonly CommandBus $commandBus,
+        private readonly QueryBus $queryBus,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setName(self::NAME)
+            ->setDescription('Computes results for all events in the list.')
+            ->addArgument(self::ARGUMENT_CHAMPIONSHIP, InputArgument::OPTIONAL, 'The series name.')
+            ->addArgument(self::ARGUMENT_YEAR, InputArgument::OPTIONAL, 'The season year.')
+            ->addOption(self::OPTION_EVENT, null, InputOption::VALUE_OPTIONAL, 'The event filter. Leave empty to sync the whole season.', false)
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $ui = new SymfonyStyle($input, $output);
+
+        $series      = $this->stringFromArgumentsOrPrompt($input, $output, self::ARGUMENT_CHAMPIONSHIP, self::QUESTION_CHAMPIONSHIP);
+        $year        = $this->intFromArgumentsOrPrompt($input, $output, self::ARGUMENT_YEAR, self::QUESTION_YEAR);
+        $eventFilter = $this->getEventFilterFromCommandOption($input);
+
+        try {
+            $response = $this->queryBus->ask(GetSeasonEventIdListQuery::fromScalars($series, $year, $eventFilter));
+            assert($response instanceof GetSeasonEventIdListResponse);
+        } catch (Throwable $e) {
+            $ui->error("Failed to get events: {$e->getMessage()}");
+
+            return Command::FAILURE;
+        }
+
+        foreach ($response->eventIdList()->idList() as $id) {
+            $ui->info("Syncing results for event {$id}");
+
+            $this->commandBus->execute(UpdateEventResultsCacheCommand::fromScalars($id));
+        }
+
+        return Command::SUCCESS;
+    }
+
+    private function getEventFilterFromCommandOption(InputInterface $input): ?string
+    {
+        $event = $input->getOption(self::OPTION_EVENT);
+        if (empty($event)) {
+            $event = null;
+        }
+
+        assert(null === $event || is_string($event));
+
+        return $event;
+    }
+}
