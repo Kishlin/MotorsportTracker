0a9fe9ba0061fe3e7fad8a039d7b85e29fabcbcd
Kishlin <hello@pierrelouislegrand.com>
2023-11-01 19:27:53 +0100

    Feature: Fix the name of the FormulaE series

--- src/Backend/MotorsportETL/Series/Application/ScrapSeriesList/TransformerSteps/MessedUpSeriesFixer.php
+++ src/Backend/MotorsportETL/Series/Application/ScrapSeriesList/TransformerSteps/MessedUpSeriesFixer.php
@@ -10,12 +10,21 @@ final class MessedUpSeriesFixer implements TransformerStep
 {
     private const ID_F1_ESPORTS_VIRTUAL_GRAND_PRIX = 'd7412896-fb6d-4a91-a928-b2ec0d54352a';
 
+    private const ID_FORMULA_E = '7415d1ab-c31d-49dd-9143-1e7e33bff889';
+
     public function transform(array $series): array
     {
         if (self::ID_F1_ESPORTS_VIRTUAL_GRAND_PRIX === $series['uuid']) {
             return [
                 ...$series,
                 'shortName' => 'eF1',
+            ];
+        }
+
+        if (self::ID_FORMULA_E === $series['uuid']) {
+            return [
+                ...$series,
+                'name' => 'ABB FIA Formula E World Championship',
             ];
         }
 
