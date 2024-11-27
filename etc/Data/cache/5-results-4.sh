d8ccf8987acd519557bb8139275c920fd1e478a8
Kishlin <hello@pierrelouislegrand.com>
2024-11-27 00:14:58 +0000

    Data: Split the results and graphs CLIs

---
+++ etc/Data/cache/5-results-4.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+#if [ $# -lt 2 ]
+#then
+#    echo "Usage: $(basename "$0" "$1") COMMAND CHAMPIONSHIP"
+#    echo "Example: $(basename "$0" "$1") \"docker-compose exec backoffice bin/console kishlin:motorsport-etl:season:scrap\" \"Formula One\""
+#    exit 2
+#fi
+
+#start=2020
+#if [[ "$3" != "" ]]; then
+#    start="$3"
+#fi
+#
+#end=2023
+#if [[ "$4" != "" ]]; then
+#    end="$4"
+#fi
+#
+#target=$1
+#championship=$2
+#
+#for year in $(seq "$start" "$end")
+#do
+#  $target "$championship" "$year"
+#done
+
+files=( 
+	"24 Hours of Le Mans"
+	"24 Hours of Daytona"
+	"ADAC 24h Rennen Nürburgring"
+)
+
+for value in "${files[@]}"
+do
+	php /app/MotorsportTracker/apps/Backoffice/bin/console kishlin:motorsport-cache:results:compute "$value"
+done
---
+++ etc/Data/cache/6-graphs-6.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+#if [ $# -lt 2 ]
+#then
+#    echo "Usage: $(basename "$0" "$1") COMMAND CHAMPIONSHIP"
+#    echo "Example: $(basename "$0" "$1") \"docker-compose exec backoffice bin/console kishlin:motorsport-etl:season:scrap\" \"Formula One\""
+#    exit 2
+#fi
+
+#start=2020
+#if [[ "$3" != "" ]]; then
+#    start="$3"
+#fi
+#
+#end=2023
+#if [[ "$4" != "" ]]; then
+#    end="$4"
+#fi
+#
+#target=$1
+#championship=$2
+#
+#for year in $(seq "$start" "$end")
+#do
+#  $target "$championship" "$year"
+#done
+
+files=( 
+	"F4 French Championship"
+	"F1 Academy"
+	"GT World Challenge Europe Endurance"
+)
+
+for value in "${files[@]}"
+do
+	php /app/MotorsportTracker/apps/Backoffice/bin/console kishlin:motorsport-cache:graphs:compute "$value"
+done
