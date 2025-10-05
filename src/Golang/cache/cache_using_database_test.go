653f15ff323719dd94aae7c0f12c16707c3e9fab
Pierre-Louis Legrand <hello@pierrelouislegrand.com>
2025-10-05 02:03:03 +0200

    Feature: Use testify/suite to create test suites

--- src/Golang/cache/cache_using_database_test.go
+++ src/Golang/cache/cache_using_database_test.go
@@ -2,118 +2,82 @@ package cache
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"testing"
 
 	"github.com/kishlin/MotorsportTracker/src/Golang/database"
 	"github.com/kishlin/MotorsportTracker/src/Golang/env"
 	_func "github.com/kishlin/MotorsportTracker/src/Golang/func"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestDatabaseCache_GetOnEmptyCache(t *testing.T) {
-	namespace, key := "series", "test_key"
-	ctx := context.Background()
+type CacheUsingDatabaseFunctionalTestSuite struct {
+	suite.Suite
 
-	cache, cleanDB := setupDatabaseCache(t, ctx)
-	defer cleanDB()
-
-	actual, hit, err := cache.Get(namespace, key)
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-	if hit {
-		t.Error("expected hit to be false, got true")
-	}
-	if actual != nil {
-		t.Errorf("expected nil value, got %v", actual)
-	}
+	cache *DatabaseCache
 }
 
-func TestDatabaseCache_GetOnExistingCache(t *testing.T) {
-	namespace, key := "series", "test_key"
-	value := []byte("test_value")
-	ctx := context.Background()
-
-	cache, cleanDB := setupDatabaseCache(t, ctx,
-		withExistingCache(namespace, key, value),
-	)
-	defer cleanDB()
-
-	actual, hit, err := cache.Get(namespace, key)
-	if err != nil {
-		t.Fatalf("unexpected error on Search: %v", err)
-	}
-	if hit == false {
-		t.Error("expected hit to be true, got false")
-	}
-	if string(value) != string(actual) {
-		t.Errorf("expected 'test_value', got %s", value)
-	}
-}
-
-func TestDatabaseCache_SetExisting(t *testing.T) {
-	namespace, key := "series", "test_key"
-	value := []byte("test_value")
-	ctx := context.Background()
-
-	cache, cleanDB := setupDatabaseCache(t, ctx)
-	defer cleanDB()
-
-	err := cache.Set(namespace, key, value)
-	if err != nil {
-		t.Fatalf("unexpected error on first Set: %v", err)
-	}
-
-	actual, hit, err := cache.Get(namespace, key)
-	if err != nil {
-		t.Fatalf("unexpected error on first Get: %v", err)
-	}
-	if hit == false {
-		t.Error("expected hit to be true on first Get, got false")
-	}
-	if string(value) != string(actual) {
-		t.Errorf("expected 'test_value' on first Get, got %s", value)
-	}
-}
-
-type databaseCacheSetupOpt func(ctx context.Context, db database.Database)
-
-func setupDatabaseCache(t *testing.T, ctx context.Context, opts ...databaseCacheSetupOpt) (cache *DatabaseCache, cleanDB func()) {
-	t.Helper()
-
+func (suite *CacheUsingDatabaseFunctionalTestSuite) SetupSuite() {
 	env.OverrideAppEnv("tests")
 	_func.Must(env.LoadEnv())
 
 	db := database.NewPGXPoolAdapter(os.Getenv("POSTGRES_CLIENT_CACHE_URL"))
 	err := db.Connect(context.Background())
-	if err != nil {
-		t.Fatalf("unable to connect to database: %v", err)
-	}
+	require.NoError(suite.T(), err)
 
-	for _, opt := range opts {
-		opt(ctx, db)
-	}
-
-	cache = NewDatabaseCache(db)
-	cleanDB = func() {
-		//goland:noinspection SqlWithoutWhere
-		err = db.Exec(ctx, "DELETE FROM series")
-		if err != nil {
-			t.Fatalf("unable to clean database: %v", err)
-		}
-		db.Close()
-	}
-
-	return cache, cleanDB
+	suite.cache = NewDatabaseCache(db)
 }
 
-func withExistingCache(namespace, key string, value []byte) databaseCacheSetupOpt {
-	return func(ctx context.Context, db database.Database) {
-		query := fmt.Sprintf(
-			"INSERT INTO %s (key, value) VALUES ($1, $2)",
-			namespace,
-		)
-		_func.Must(db.Exec(ctx, query, key, value))
-	}
+func (suite *CacheUsingDatabaseFunctionalTestSuite) TearDownSuite() {
+	suite.cache.db.Close()
+}
+
+var setupQuery = "INSERT INTO series (key, value) VALUES ('existing_key', 'existing_value')"
+
+func (suite *CacheUsingDatabaseFunctionalTestSuite) SetupTest() {
+	err := suite.cache.db.Exec(context.Background(), setupQuery)
+	require.NoError(suite.T(), err)
+}
+
+func (suite *CacheUsingDatabaseFunctionalTestSuite) TearDownTest() {
+	//goland:noinspection SqlWithoutWhere
+	err := suite.cache.db.Exec(context.Background(), "DELETE FROM series")
+	require.NoError(suite.T(), err)
+}
+
+func (suite *CacheUsingDatabaseFunctionalTestSuite) TestGet() {
+	actual, hit, err := suite.cache.Get("series", "missing_key")
+	require.NoError(suite.T(), err)
+	require.False(suite.T(), hit)
+	require.Nil(suite.T(), actual)
+
+	actual, hit, err = suite.cache.Get("series", "existing_key")
+	require.NoError(suite.T(), err)
+	require.True(suite.T(), hit)
+	require.Equal(suite.T(), "existing_value", string(actual))
+}
+
+func (suite *CacheUsingDatabaseFunctionalTestSuite) TestSet() {
+	// Test adding new key
+	err := suite.cache.Set("series", "new_key", []byte("test_value"))
+	require.NoError(suite.T(), err)
+
+	actual, hit, err := suite.cache.Get("series", "new_key")
+	require.NoError(suite.T(), err)
+	require.True(suite.T(), hit)
+	require.Equal(suite.T(), "test_value", string(actual))
+
+	// Test updating existing key
+	err = suite.cache.Set("series", "existing_key", []byte("updated_value"))
+	require.NoError(suite.T(), err)
+
+	actual, hit, err = suite.cache.Get("series", "existing_key")
+	require.NoError(suite.T(), err)
+	require.True(suite.T(), hit)
+	require.Equal(suite.T(), "updated_value", string(actual))
+}
+
+func TestFunctional_CacheUsingDatabase(t *testing.T) {
+	suite.Run(t, new(CacheUsingDatabaseFunctionalTestSuite))
 }
