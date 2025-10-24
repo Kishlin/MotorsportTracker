98b4be23a13c2491b2d0eddb29076d0e7b802257
Pierre-Louis Legrand <hello@pierrelouislegrand.com>
2025-10-24 20:35:05 +0200

    Feature: Detect env vars from working directory

--- src/Golang/shared/env/infrastructure/env.go
+++ src/Golang/shared/env/infrastructure/env.go
@@ -1,8 +1,11 @@
 package infrastructure
 
 import (
+	"errors"
 	"fmt"
+	"io/fs"
 	"os"
+	"path/filepath"
 
 	"github.com/joho/godotenv"
 
@@ -13,7 +16,7 @@ var allowedEnvs = []string{"dev", "tests", "production"}
 
 const appEnvKey = "APP_ENV"
 
-func LoadEnv() error {
+func LoadEnv() (err error) {
 	env := getEnv()
 	if envIsValid(env) == false {
 		return fmt.Errorf("invalid APP_ENV value: %s", env)
@@ -21,7 +24,10 @@ func LoadEnv() error {
 
 	projectDir := os.Getenv("PROJECT_DIR")
 	if projectDir == "" {
-		projectDir = "."
+		projectDir, err = findProjectDir()
+		if err != nil {
+			return fmt.Errorf("looking for project dir: %w", err)
+		}
 	}
 
 	fmt.Println("Project Dir: " + projectDir)
@@ -44,6 +50,45 @@ func OverrideAppEnv(env string) func() {
 	return func() {
 		fn.Must(os.Setenv(appEnvKey, initialValue))
 	}
+}
+
+func findProjectDir() (string, error) {
+	candidate, err := os.Getwd()
+	if err != nil {
+		return "", fmt.Errorf("finding executable directory: %w", err)
+	}
+
+	maxDepth := 10
+
+	for i := 0; i < maxDepth; i++ {
+		hasEnvFile, err := exists(filepath.Join(candidate, ".env"))
+		if err != nil {
+			return "", fmt.Errorf("checking whether dir %s has an .env file: %w", candidate, err)
+		}
+		if hasEnvFile {
+			return candidate, nil
+		}
+
+		parent := filepath.Dir(candidate)
+		if parent == candidate {
+			break
+		}
+		candidate = parent
+	}
+
+	return "", fmt.Errorf(".env file not found (searched up to %d levels to %s)", maxDepth, candidate)
+}
+
+func exists(path string) (bool, error) {
+	fmt.Println("Checking existence of .env file:", path)
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if errors.Is(err, fs.ErrNotExist) {
+		return false, nil
+	}
+	return false, err
 }
 
 func getEnv() string {
